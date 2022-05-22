0311612504d22f30e2e07a2abad9839459acd7ec
noramr <105988830+noramr@users.noreply.github.com>
2022-05-22 17:52:28 +0200

    Create practica-marquez.php

---
+++ practica-marquez.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author Nora
+ * @version 1.0
+*/
+
+/**
+ * Función suma de dos números
+ * @param int $num1
+ * @param int $num2
+ * @return float
+*/
+
+function suma ($num1, $num2) {
+	$resultadosuma = $num1 + $num2;
+	return $resultadosuma;
+}
+$suma = suma (24,42);
+echo $suma;
+?>
